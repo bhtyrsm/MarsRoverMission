5e72daaa98ab76bb93b1d17e9bacb7517df43950
Bahtiyar <bahtiyarsime@gmail.com>
2020-12-20 16:24:16 +0300

    Create Project
    Create Project

---
+++ MarsRoverMission/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsRoverMission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
