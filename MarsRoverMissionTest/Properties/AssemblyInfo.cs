78b8c1442ab6dc6e0c754c2c64a2893aaad5d774
Bahtiyar <bahtiyarsime@gmail.com>
2020-12-20 17:41:13 +0300

    Add UnitTest
    Add unittest project

---
+++ MarsRoverMissionTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MarsRoverMissionTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Turkcell Iletisim Hizmetleri A.S.")]
+[assembly: AssemblyProduct("MarsRoverMissionTest")]
+[assembly: AssemblyCopyright("Copyright © Turkcell Iletisim Hizmetleri A.S. 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d23c7338-ea35-4cb5-82c1-4f56ac4c327a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
