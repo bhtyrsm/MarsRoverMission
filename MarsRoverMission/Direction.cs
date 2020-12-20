3fe7c03a4654f5fa0ffcb9d8332fd7882b0898e0
Bahtiyar <bahtiyarsime@gmail.com>
2020-12-20 16:30:45 +0300

    Direction
    Add direction file

---
+++ MarsRoverMission/Direction.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsRoverMission
+{
+    /// <summary>
+    /// Directions
+    /// </summary>
+    public enum Direction
+    {
+        /// <summary>
+        /// Nourth
+        /// </summary>
+        N = 1,
+
+        /// <summary>
+        /// South
+        /// </summary>
+        S = 2,
+
+        /// <summary>
+        /// East
+        /// </summary>
+        E = 3,
+
+        /// <summary>
+        /// West
+        /// </summary>
+        W = 4
+    }
+}
